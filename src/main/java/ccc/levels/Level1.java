a5560a7a731e9657d540e75264a39e04bec2e92d
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-10-25 10:18:53 +0200

    Fix Level 1

--- src/main/java/ccc/levels/Level1.java
+++ src/main/java/ccc/levels/Level1.java
@@ -7,11 +7,13 @@ public class Level1 extends LevelImpl {
     public String perform(String input) {
         String returnValue = "";
         String[] a = input.split("\n");
-        for (String s : a) {
+        for (int i = 1; i < a.length; i ++) {
+            String s = a[i];
             int value = 0;
-            String[] b = s.split(" ");
+            String[] b = s.split("\\s+");
             for (String s2 : b) {
-                value += Integer.parseInt(s2.replaceAll("\r", ""));
+                value += Integer.parseInt(s2);
+
             }
             returnValue += value + "\n";
         }
