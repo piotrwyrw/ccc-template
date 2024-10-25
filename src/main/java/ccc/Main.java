638808ae5561143502619c272db0d24577635bf7
RastiPatak <pat210600@spengergasse.at>
2024-10-25 10:28:00 +0200

    level2

--- src/main/java/ccc/Main.java
+++ src/main/java/ccc/Main.java
@@ -1,12 +1,11 @@
 package ccc;
 
-import ccc.levels.Level1;
-import ccc.levels.SampleLevel;
+import ccc.levels.Level2;
 
 public class Main {
 
     public static void main(String[] args) {
-        Levels.invoke(Level1.class, "level_1");
+        Levels.invoke(Level2.class, "level_2");
     }
 
 }
--- src/main/java/ccc/levels/Level2.java
+++ src/main/java/ccc/levels/Level2.java
@@ -2,20 +2,21 @@ package ccc.levels;
 
 import ccc.LevelImpl;
 
-public class Level1 extends LevelImpl {
+public class Level2 extends LevelImpl {
     @Override
     public String perform(String input) {
         String returnValue = "";
         String[] a = input.split("\n");
         for (int i = 1; i < a.length; i ++) {
+            int velocity = 0;
             String s = a[i];
-            int value = 0;
+            int height = 0;
             String[] b = s.split("\\s+");
             for (String s2 : b) {
-                value += Integer.parseInt(s2);
-
+                velocity += Integer.parseInt(s2) - 10;
+                height += velocity;
             }
-            returnValue += value + "\n";
+            returnValue += height + "\n";
         }
         return returnValue;
     }
